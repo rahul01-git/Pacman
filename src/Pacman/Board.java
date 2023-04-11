82478d8e97e9adf0180503b80d65c61485e39ec1
rahul01-git <rahulneupane0090@gmail.com>
2023-04-11 22:03:18 +0545

    initial setup

---
+++ src/Pacman/Board.java
@@ -0,0 +1,4 @@
+package Pacman;
+
+public class Board {
+}
---
+++ src/Pacman/Pacman.java
@@ -0,0 +1,7 @@
+package Pacman;
+
+public class Pacman {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
